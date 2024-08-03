80ca82c91a217b275c1cdfbafbd95b80be5e00cb
htawa <haitailook@outlook.com>
2024-08-04 00:34:17 +0800

    v1.0.0

---
+++ src/main/java/cn/tlgjx/lunkun/LunKunClient.java
@@ -0,0 +1,9 @@
+package cn.tlgjx.lunkun;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LunKunClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cn/tlgjx/lunkun/item/Moditems.java
@@ -0,0 +1,17 @@
+package cn.tlgjx.lunkun.item;
+
+import cn.tlgjx.lunkun.LunKun;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class Moditems {
+    public static final Item LUNKUN_DEBRIS=registerModItems("lunkun_debris",new Item(new FabricItemSettings()));
+    private static Item registerModItems(String name,Item item){
+        return Registry.register(Registries.ITEM,new Identifier(LunKun.MOD_ID,name),item);
+    }
+    public static void registerModItems(){
+    }
+}
