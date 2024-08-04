52fc592fb61d3def662a2e0eaf0cbec93dad79a9
htawa <haitailook@outlook.com>
2024-08-04 16:14:07 +0800

    lunkun_ingot and lunkun_block beta
    Signed-off-by: htawa <haitailook@outlook.com>

---
+++ src/main/java/cn/tlgjx/lunkun/block/ModBlocks.java
@@ -0,0 +1,26 @@
+package cn.tlgjx.lunkun.block;
+
+import cn.tlgjx.lunkun.LunKun;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static final Block LUNKUN_BLOCK=registerBlocks("lunkun_block",new Block(FabricBlockSettings.copyOf(Blocks.MAGMA_BLOCK)));
+    private static Block registerBlocks(String name,Block block){
+        registerBlocksItem(name,block);
+        return Registry.register(Registries.BLOCK,new Identifier(LunKun.MOD_ID,name),block);
+    }
+    private static Item registerBlocksItem(String name,Block block){
+        return Registry.register(Registries.ITEM,new Identifier(LunKun.MOD_ID,name),new BlockItem(block,new FabricItemSettings()));
+    }
+    public static void registerModBlocks(){
+
+    }
+}
--- src/main/java/cn/tlgjx/lunkun/item/Moditems.java
+++ src/main/java/cn/tlgjx/lunkun/item/Moditems.java
@@ -9,6 +9,7 @@ import net.minecraft.util.Identifier;
 
 public class Moditems {
     public static final Item LUNKUN_DEBRIS=registerModItems("lunkun_debris",new Item(new FabricItemSettings()));
+    public static final Item LUNKUN_INGOT=registerModItems("lunkun_ingot",new Item(new FabricItemSettings()));
     private static Item registerModItems(String name,Item item){
         return Registry.register(Registries.ITEM,new Identifier(LunKun.MOD_ID,name),item);
     }
